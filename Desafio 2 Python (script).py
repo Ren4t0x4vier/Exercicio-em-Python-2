5169bcb6aa1de48ee0a2da6b7bf86fda4c30caa3
Renato Xavier <126256872+Ren4t0x4vier@users.noreply.github.com>
2023-02-25 18:56:26 -0400

    Add files via upload

---
+++ Desafio 2 Python (script).py
@@ -0,0 +1,9 @@
+print("Olá, seja bem vindo(a)")
+
+dia=input("Em que dia você nasceu? " )
+
+mes=input("Em que mês você nasceu? " )
+
+ano=input("Em que ano você nasceu? " )
+
+print("Sua data de nascimento é",dia,"/", mes,"/", ano)
